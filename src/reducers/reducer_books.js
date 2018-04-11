cbeae134d48df59998addffad11ff80f51f71ef7
karan <karancheema1@yahoo.com>
2018-04-10 17:16:00 -0700

    first push

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Book1', pages: 98},
+    {title: 'Book2', pages: 123},
+    {title: 'Book3', pages: 35},
+    {title: 'Book4', pages: 212}
+  ]
+};
